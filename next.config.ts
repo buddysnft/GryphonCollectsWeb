74f1df2dfd703aa1680343be6a27f8d7a07e04c7
Jon Allen <jonbot@mac-mini.mynetworksettings.com>
2026-03-02 13:23:53 -0500

    fix: allow Unsplash images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
     ],
   },
 };
